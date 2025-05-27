eafd5434b4f08d7fa1f0e1c1ef39d6a2ae70a804
SampadaPawate <sampadapawate@gmail.com>
2025-05-26 22:17:03 -0500

    implement generation page ui

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,22 +1,20 @@
 import Agent from "@/components/Agent";
-import { getCurrentUser } from "@/lib/actions/auth.action";
+
 
 const Page = async () => {
-    const user = await getCurrentUser();
 
     return (
         <>
             <h3>Interview generation</h3>
 
             <Agent
-                userName={user?.name!}
-                userId={user?.id}
-                profileImage={user?.profileURL}
+                userName="You"
+                userId="user1"
                 type="generate"
             />
         </>
-    );
-};
+    )
+}
 
 export default Page;
 
